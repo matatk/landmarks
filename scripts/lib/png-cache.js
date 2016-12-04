ae9e5792b7c7834fc8d900198b7c79178b50fecf
Matthew Tylee Atkinson <matatk@gmail.com>
2016-12-04 18:20:32 +0000

    Switch to using fs-extra for png-cache

--- scripts/lib/png-cache.js
+++ scripts/lib/png-cache.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const path = require('path')
-const fs = require('fs')
+const fse = require('fs-extra')
 const chalk = require('chalk')
 const svg2png = require('svg2png')
 
@@ -13,34 +13,34 @@ module.exports = function(cacheDir, svgPath) {
 
 	// Check if a (PNG) file is newer than the SVG file
 	function isOlderThanSvg(pngPath) {
-		return fs.statSync(pngPath).mtime < svgModified
+		return fse.statSync(pngPath).mtime < svgModified
 	}
 
 	// Check if (PNG) file either doesn't exist or is outdated
 	function isPngAbsentOrOutdated(pngPath) {
-		return !fs.existsSync(pngPath) || isOlderThanSvg(pngPath)
+		return !fse.existsSync(pngPath) || isOlderThanSvg(pngPath)
 	}
 
 	// Generate PNG from SVG
 	function generatePng(size, outputPath) {
 		console.log(chalk.bold.blue(`Generating ${outputPath}...`))
-		const svgBuffer = fs.readFileSync(svgPath)
+		const svgBuffer = fse.readFileSync(svgPath)
 		const pngBuffer = svg2png.sync(svgBuffer, {
 			width: size,
 			height: size
 		})
-		fs.writeFileSync(outputPath, pngBuffer)
+		fse.writeFileSync(outputPath, pngBuffer)
 	}
 
 
 	// Initialisation
-	const svgModified = fs.statSync(svgPath).mtime
+	const svgModified = fse.statSync(svgPath).mtime
+	fse.ensureDirSync(cacheDir)
 
-	if (!fs.existsSync(cacheDir)) {
-		fs.mkdirSync(cacheDir)
-	}
-
+	// Public API implementation
 	return {
+		// Get the path for a PNG of a particular size.
+		// If it does not exist, or if it is out-of-date, (re-)generate it.
 		getPngPath: function(size) {
 			const requestedPngPath = pngPath(size)
 			if (isPngAbsentOrOutdated(requestedPngPath)) {
