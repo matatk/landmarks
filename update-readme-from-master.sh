4f95d30be026db7612343719e2b8499ee531ca0a
Matthew Tylee Atkinson <matatk@gmail.com>
2016-12-05 20:32:43 +0000

    Move the script that synchs README changes to gh-pages branch

---
+++ update-readme-from-master.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+LAYOUT_PREFIX='---\r\nlayout: index\r\n---\r\n'
+git checkout master -- README.md
+echo -e $LAYOUT_PREFIX > index.md
+cat README.md | tail -n +5 >> index.md
+git rm --force README.md
+git add index.md
+git commit -m "Sync README.md in master branch to index.md in gh-pages"
+git push origin gh-pages
+# Thanks http://stackoverflow.com/a/16389663/1485308 :-)
