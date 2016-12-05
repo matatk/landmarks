6fd64a41353b653137b57088ec309f59caf9d18c
Matthew Tylee Atkinson <matatk@gmail.com>
2016-12-05 20:48:16 +0000

    Get rid of extra blank line when synching README

--- update-readme-from-master.sh
+++ update-readme-from-master.sh
@@ -2,7 +2,7 @@
 LAYOUT_PREFIX='---\r\nlayout: index\r\n---\r\n'
 git checkout master -- README.md
 echo -e $LAYOUT_PREFIX > index.md
-cat README.md | tail -n +5 >> index.md
+cat README.md | tail -n +6 >> index.md
 git rm --force README.md
 git add index.md
 git commit -m "Sync README.md in master branch to index.md in gh-pages"
