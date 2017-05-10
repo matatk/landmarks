2e996a4a02ebdc6aef9097f4570d21723f8d531e
Matthew Tylee Atkinson <matatk@gmail.com>
2017-05-10 14:02:33 +0100

    Check all tabs wrt disabling the browser action
    This checks all the tabs to see if they are HTTP(S) pages. It therefore
    partly addresses #55—at least enough to release 2.0.7. It would be nice
    if browser-specific 'pages to avoid' lists could be added, though.

--- src/assemble/injector.js
+++ src/assemble/injector.js
@@ -3,7 +3,7 @@
 function landmarksContentScriptInjector() {
 	// Inject content script manually
 	browser.tabs.query({}, function(tabs) {
-		for(const i in tabs) {
+		for (const i in tabs) {
 			if (/^https?:\/\//.test(tabs[i].url)) {
 				browser.tabs.executeScript(tabs[i].id, {
 					file: 'compatibility.js'
