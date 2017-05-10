fc2e1a31697f3d1c331f2baee3c37c60a859a7b4
Matthew Tylee Atkinson <matatk@gmail.com>
2017-05-10 13:40:40 +0100

    Permissions
    * Include a content script injector helper on !Firefox.
    * Use the content script injector on !Firefox.
    * Tweak manifests accordingly.

---
+++ src/assemble/injector.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function landmarksContentScriptInjector() {
+	// Inject content script manually
+	browser.tabs.query({}, function(tabs) {
+		for(const i in tabs) {
+			if (/^https?:\/\//.test(tabs[i].url)) {
+				browser.tabs.executeScript(tabs[i].id, {
+					file: 'compatibility.js'
+				}, function() {
+					browser.tabs.executeScript(tabs[i].id, {
+						file: 'content.finder.js'
+					}, function() {
+						browser.tabs.executeScript(tabs[i].id, {
+							file: 'content.focusing.js'
+						}, function() {
+							browser.tabs.executeScript(tabs[i].id, {
+								file: 'content.management.js'
+							})
+						})
+					})
+				})
+			}
+		}
+	})
+}
--- src/static/background.js
+++ src/static/background.js
@@ -1,5 +1,5 @@
 'use strict'
-/* global sendToActiveTab */
+/* global sendToActiveTab landmarksContentScriptInjector */
 
 //
 // Keyboard Shortcut Handling
@@ -119,28 +119,10 @@ function landmarksBadgeUpdate(tabId, numberOfLandmarks) {
 
 browser.runtime.onInstalled.addListener(function(details) {
 	if (details.reason === 'install' || details.reason === 'update') {
-		// Inject content script manually
-		browser.tabs.query({}, function(tabs) {
-			for(const i in tabs) {
-				if (/^https?:\/\//.test(tabs[i].url)) {
-					browser.tabs.executeScript(tabs[i].id, {
-						file: 'compatibility.js'
-					}, function() {
-						browser.tabs.executeScript(tabs[i].id, {
-							file: 'content.finder.js'
-						}, function() {
-							browser.tabs.executeScript(tabs[i].id, {
-								file: 'content.focusing.js'
-							}, function() {
-								browser.tabs.executeScript(tabs[i].id, {
-									file: 'content.management.js'
-								})
-							})
-						})
-					})
-				}
-			}
-		})
+		// Don't inject the content script on Firefox
+		if (typeof landmarksContentScriptInjector !== 'undefined') {
+			landmarksContentScriptInjector()
+		}
 
 		// Show website and get it to display an appropriate notice
 		const baseUrl = 'http://matatk.agrip.org.uk/landmarks/#!'
