3315812155c0a748ff7e658320bd60f95e9f4a1d
Matthew Tylee Atkinson <matatk@gmail.com>
2018-02-17 12:25:28 +0000

    Translation of roles; Test data
    * Fix lookup of doc-* roles.
    * Indicate how the book could be marked up more appropriately, and why it is not here.

--- src/static/popup.js
+++ src/static/popup.js
@@ -142,7 +142,7 @@ function processRole(role) {
 	const capRole = base => (base.charAt(0).toUpperCase() + base.slice(1))
 
 	return browser.i18n.getMessage('role' +
-		(role.startsWith('doc-') ? capRole(role.slice(5)) : capRole(role)))
+		(role.startsWith('doc-') ? capRole(role.slice(4)) : capRole(role)))
 }
 
 // When a landmark's corresponding button in the UI is clicked, focus it
