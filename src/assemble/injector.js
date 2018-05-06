9b1f83ad5a1b5c50a59eb520cdae32a6b72e6cc2
Matthew Tylee Atkinson <matatk@gmail.com>
2018-05-06 13:01:09 +0100

    Don't inject the content script into special pages

--- src/assemble/injector.js
+++ src/assemble/injector.js
@@ -1,11 +1,25 @@
 'use strict'
 /* exported landmarksContentScriptInjector */
+/* global specialPages */
 
 function landmarksContentScriptInjector() {
 	// Inject content script manually
 	browser.tabs.query({}, function(tabs) {
 		for (const i in tabs) {
 			if (/^(https?|file):\/\//.test(tabs[i].url)) {  // TODO DRY
+				// Don't inject the content script into special pages. (Opera
+				// now appears to treat this as a blocking error, at least with
+				// unpacked extensions, and performing this check is cricket
+				// anyway.)
+				let skipThisTab = false
+				for (const specialPage of specialPages) {
+					if (specialPage.test(tabs[i].url)) {
+						skipThisTab = true
+						break
+					}
+				}
+				if (skipThisTab) continue
+
 				browser.tabs.executeScript(tabs[i].id, {
 					file: 'compatibility.js'
 				}, function() {
