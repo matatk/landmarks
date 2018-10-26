38cd3de3ef9321783db0183e3d1feddb604a3975
Matthew Tylee Atkinson <matatk@gmail.com>
2018-10-26 18:34:56 +0100

    Remove "show all landmarks" command for 2.4.0
    This will be back...
    
    Ref #120.

--- src/code/_background.js
+++ src/code/_background.js
@@ -285,7 +285,6 @@ browser.commands.onCommand.addListener(function(command) {
 		case 'next-landmark':
 		case 'prev-landmark':
 		case 'main-landmark':
-		case 'show-all-landmarks':
 			sendToActiveContentScriptIfExists({ name: command })
 			break
 		default:
