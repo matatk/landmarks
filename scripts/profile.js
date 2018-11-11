3c6b2f826b8717a9c7de321d976d77138fe003ec
Matthew Tylee Atkinson <matatk@gmail.com>
2018-11-11 21:32:13 +0000

    Improve profiling script
    * Offer a range of a few sites.
    * To make it more predictable, landmark scans are triggered by inserting
      an element (which also happens to be a landmark) into the page.
    * The user can specify how many times this is to happen.

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,35 +1,92 @@
 'use strict'
+/* global document */
 const puppeteer = require('puppeteer')
 
-puppeteer.launch({
-	headless: false,  // needed to support extensions
-	args: [
-		'--disable-extensions-except=build/chrome/',
-		'--load-extension=build/chrome/'
-	]
-}).then(async browser => {
-	const page = await browser.newPage()
+const sites = Object.freeze({
+	ars: 'https://arstechnica.com',
+	bbc: 'https://www.bbc.co.uk/',
+	bbcnews: 'https://www.bbc.co.uk/news'
+})
 
-	await page.goto('https://www.theverge.com')
-	await page.bringToFront()
-	await page.tracing.start({path: 'trace.json', screenshots: true})
-	await page.waitFor(2000)
+const pageSettlingDelay = 4000
+const delayAfterInsertingLandmark = 1000
 
-	// Find the first category navigation link
-	const firstNavLink = await page.$('body > div.l-root.l-reskin > div.l-header > div > div.c-masthead.c-masthead--centered > header > div > nav > ul > li:nth-child(1) > a')
-	const box = await firstNavLink.boundingBox()
+async function insertLandmark(page, repetition) {
+	await page.evaluate((step) => {
+		const id = `profile-injected-landmark-${step}`
 
-	const baseXoffset = 20
-	const baseYoffset = 20
+		const textNode = document.createTextNode(`Step: ${step}`)
 
-	// Move the mouse around a lot causing the menus to appear/disappear
-	for (let repetition = 0; repetition < 10; repetition++) {
-		for (let offset = baseXoffset; offset < 700; offset += 100) {
-			await page.mouse.move(box.x + offset, box.y + baseYoffset, {steps: 20})
-			await page.waitFor(350)
+		const para = document.createElement('p')
+		para.setAttribute('id', id)
+		para.appendChild(textNode)
+
+		const landmark = document.createElement('section')
+		landmark.setAttribute('aria-labelledby', id)
+		landmark.appendChild(para)
+
+		document.body.insertBefore(landmark, document.body.firstChild)
+	}, repetition)
+}
+
+function orchestrateBrowser(name, url, loops) {
+	puppeteer.launch({
+		headless: false,  // needed to support extensions
+		args: [
+			'--disable-extensions-except=build/chrome/',
+			'--load-extension=build/chrome/'
+		]
+	}).then(async browser => {
+		const page = await browser.newPage()
+
+		await page.tracing.start({
+			path: `trace-${name}-${loops}.json`,
+			screenshots: true
+		})
+
+		await page.goto(url, { waitUntil: 'domcontentloaded' })
+		await page.bringToFront()
+
+		console.log('Page loaded; settling...')
+		await page.waitFor(pageSettlingDelay)
+
+		console.log('Repetition stage (if applicable)...')
+		for (let repetition = 0; repetition < loops; repetition++) {
+			console.log(`Repetition ${repetition}`)
+			await insertLandmark(page, repetition)
+			await page.waitFor(delayAfterInsertingLandmark)
 		}
+
+		console.log('Stopping...')
+		await page.tracing.stop()
+		await browser.close()
+	})
+}
+
+function usageAndExit() {
+	console.error('Usage: npm run profile -- <site> <repetitions>')
+	console.error('       <repetitions>: extra number of times to scan')
+	console.error('Valid sites:\n', sites)
+	process.exit(42)
+}
+
+function main() {
+	const siteName = process.argv[2]
+	if (!siteName) usageAndExit()
+
+	const siteUrl = sites[siteName]
+	if (!siteUrl) {
+		console.error(`Unkown site "${siteName}".`)
+		usageAndExit()
 	}
 
-	await page.tracing.stop()
-	await browser.close()
-})
+	const loops = Number(process.argv[3])
+	if (isNaN(loops)) {
+		console.error(`Invalid number of repetitions "${process.argv[3]}".`)
+		usageAndExit()
+	}
+
+	orchestrateBrowser(siteName, siteUrl, loops)
+}
+
+main()
