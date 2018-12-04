d4b315c0d7518ab5905ad69a45e5a2613dcd44f4
Matthew Tylee Atkinson <matatk@gmail.com>
2018-12-04 09:09:55 +0000

    Support profiling just LandmarksFinder.find()
    This adds another mode to the profiling script that does not load the
    extension, rather only the LandmarksFinder code, and runs it directly on
    the page, a variable number of times. Hopefully this will provide more
    consistent results.

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,37 +1,43 @@
 'use strict'
-/* global document */
+/* global window document */
+const path = require('path')
 const puppeteer = require('puppeteer')
 
 const sites = Object.freeze({
-	abootstrap: 'https://angular-ui.github.io/bootstrap/',  // lots of mutations
-	ars: 'https://arstechnica.com',                         // lots of mutations
-	bbc: 'https://www.bbc.co.uk/',                          // neat intervals
-	bbcnews: 'https://www.bbc.co.uk/news',                  // neat intervals
-	googledoc: 'https://docs.google.com/document/d/1GPFzG-d47qsD1QjkCCel4-Gol6v34qduFMIhBsGUSTs'                                            // neat intervals
+	abootstrap: 'https://angular-ui.github.io/bootstrap/',
+	ars: 'https://arstechnica.com',
+	bbc: 'https://www.bbc.co.uk/',
+	bbcnews: 'https://www.bbc.co.uk/news',
+	googledoc: 'https://docs.google.com/document/d/1GPFzG-d47qsD1QjkCCel4-Gol6v34qduFMIhBsGUSTs'
 })
 
-const pageSettlingDelay = 4e3
-const delayAfterInsertingLandmark = 1e3
+const pageSettlingDelay = 2e3
 
-async function insertLandmark(page, repetition) {
-	await page.evaluate((step) => {
-		const id = `profile-injected-landmark-${step}`
 
-		const textNode = document.createTextNode(`Step: ${step}`)
+//
+// Inserting landmarks into a page over time
+//
 
-		const para = document.createElement('p')
-		para.setAttribute('id', id)
-		para.appendChild(textNode)
+function setUpLandmarkRuns(siteName, siteUrl) {
+	// FIXME DRY
+	const landmarks = Number(process.argv[4])
+	if (isNaN(landmarks) || landmarks < 0) {
+		console.error(`Invalid number of landmarks "${process.argv[4]}".`)
+		usageAndExit()
+	}
 
-		const landmark = document.createElement('section')
-		landmark.setAttribute('aria-labelledby', id)
-		landmark.appendChild(para)
+	const runs = Number(process.argv[5])
+	if (isNaN(runs) || runs < 1) {
+		console.error(`Invalid number of runs "${process.argv[5]}".`)
+		usageAndExit()
+	}
 
-		document.body.insertBefore(landmark, document.body.firstChild)
-	}, repetition)
+	doLandmarkInsertionRuns(siteName, siteUrl, landmarks, runs)
 }
 
-function orchestrateBrowser(name, url, landmarks, runs) {
+function doLandmarkInsertionRuns(name, url, landmarks, runs) {
+	const delayAfterInsertingLandmark = 1e3
+
 	puppeteer.launch({
 		headless: false,  // needed to support extensions
 		args: [
@@ -69,17 +75,105 @@ function orchestrateBrowser(name, url, landmarks, runs) {
 	})
 }
 
+async function insertLandmark(page, repetition) {
+	await page.evaluate((step) => {
+		const id = `profile-injected-landmark-${step}`
+
+		const textNode = document.createTextNode(`Step: ${step}`)
+
+		const para = document.createElement('p')
+		para.setAttribute('id', id)
+		para.appendChild(textNode)
+
+		const landmark = document.createElement('section')
+		landmark.setAttribute('aria-labelledby', id)
+		landmark.appendChild(para)
+
+		document.body.insertBefore(landmark, document.body.firstChild)
+	}, repetition)
+}
+
+
+//
+// Specific landmarksFinder.find() test
+//
+
+function profileSpecific(siteName, siteUrl) {
+	const landmarksFinderPath = path.join('scripts', 'browser-landmarks.js')
+
+	// FIXME DRY
+	const loops = Number(process.argv[4])
+	if (isNaN(loops) || loops < 1) {
+		console.error(`Invalid number of repetitions "${process.argv[4]}".`)
+		usageAndExit()
+	}
+
+	console.log(`Runing landmarks loop test on ${siteName}...`)
+	console.log(`URL: ${siteUrl}`)
+	puppeteer.launch().then(async browser => {
+		const page = await browser.newPage()
+		console.log('Loading page...')
+		await page.goto(siteUrl, { waitUntil: 'domcontentloaded' })
+		console.log('Page loaded; settling...')  // TODO DRY?
+		await page.waitFor(pageSettlingDelay)
+		console.log('Injecting script...')
+		await page.addScriptTag({
+			path: landmarksFinderPath
+		})
+
+		page.on('console', msg => console.log(msg.text()))
+
+		console.log('Running loops...')
+		const duration = await page.evaluate(repetitions => {
+			let totalDuration = 0
+			for (let i = 0; i < repetitions; i++) {
+				console.log(`Loop ${i}`)
+				const lf = new window.LandmarksFinder(window, document)
+				const start = window.performance.now()
+				lf.find()
+				const end = window.performance.now()
+				totalDuration += (end - start)
+			}
+			return totalDuration
+		}, loops)
+
+		console.log()
+		console.log('Duration:', duration)
+		console.log('Loops:', loops)
+		console.log('Mean:', duration / loops)
+		await browser.close()
+		console.log('Done.')
+	})
+}
+
+
+//
+// Main gubbins
+//
+
 function usageAndExit() {
-	console.error('Usage: npm run profile -- <site> <landmarks> <runs>')
-	console.error('       <landmarks> to insert, with a pause between each.')
-	console.error('       <runs> number of separate tracing runs to make.')
+	console.error('Usage: npm run profile -- trace <site> <landmarks> <runs>')
+	console.error('           Runs the built extension on a real site and traces for')
+	console.error('           performance, whilst inserting a number of landmarks.')
+	console.error('or:    npm run profile -- time <site> <repetitions>')
+	console.error('           Runs only the code to find landmarks specifically, for')
+	console.error('           the number of times specified.')
 	console.error('Valid <sites>:\n', sites)
 	process.exit(42)
 }
 
 function main() {
-	const siteName = process.argv[2]
-	if (!siteName) usageAndExit()
+	const mode = process.argv[2]
+	if (!mode) {
+		console.error('No mode given.')
+		usageAndExit()
+	}
+
+	const siteName = process.argv[3]
+	if (!siteName) {
+		console.error('No site given.')
+		usageAndExit()
+	}
 
 	const siteUrl = sites[siteName]
 	if (!siteUrl) {
@@ -87,19 +181,17 @@ function main() {
 		usageAndExit()
 	}
 
-	const landmarks = Number(process.argv[3])
-	if (isNaN(landmarks) || landmarks < 0) {
-		console.error(`Invalid number of landmarks "${process.argv[3]}".`)
-		usageAndExit()
+	switch (mode) {
+		case 'trace':
+			setUpLandmarkRuns(siteName, siteUrl)
+			break
+		case 'time':
+			profileSpecific(siteName, siteUrl)
+			break
+		default:
+			console.error(`Unknown mode: ${mode}`)
+			usageAndExit()
 	}
-
-	const runs = Number(process.argv[4])
-	if (isNaN(runs) || runs < 1) {
-		console.error(`Invalid number of runs "${process.argv[4]}".`)
-		usageAndExit()
-	}
-
-	orchestrateBrowser(siteName, siteUrl, landmarks, runs)
 }
 
 main()
