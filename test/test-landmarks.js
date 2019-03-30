8576f4d8a0cf6f7789f54b003c79639411a59a36
Matthew Tylee Atkinson <matatk@gmail.com>
2019-03-30 17:19:46 +0000

    Convert Landmarks-format test results to trees in test suite
    Allows Landmarks to keep its current code and data structure format, but have the tests expressed as trees, which seems more natural.

--- test/test-landmarks.js
+++ test/test-landmarks.js
@@ -8,9 +8,103 @@ const testCodePath = path.join(__dirname, 'test-code-in-harness-landmarks.js')
 const fixturesDir = path.join(__dirname, 'fixtures')
 const dataDir = path.join(__dirname, 'data')
 
+const testSuiteFormatExample = {
+	'expected': [
+		{
+			'type': 'landmark',
+			'role': 'banner',
+			'label': null,
+			'selector': 'body > header',
+			'contains': [
+				{
+					'type': 'landmark',
+					'role': 'navigation',
+					'label': 'World of wombats',
+					'selector': 'body > header > nav'
+				}
+			]
+		},
+		{
+			'type': 'landmark',
+			'role': 'main',
+			'label': 'Looking after your wombat',
+			'selector': 'body > main',
+			'contains': [
+				{
+					'type': 'landmark',
+					'role': 'navigation',
+					'label': 'Looking after your wombat Topics',
+					'selector': 'body > main > nav:nth-child(2)'
+				}
+			]
+		},
+		{
+			'type': 'landmark',
+			'role': 'contentinfo',
+			'label': null,
+			'selector': 'body > footer'
+		}
+	]
+}
+
+const landmarksFormatExample = {
+	'expected': [
+		{
+			'depth': 0,
+			'role': 'banner',
+			'label': null,
+			'selector': 'body > header'
+		},
+		{
+			'depth': 1,
+			'role': 'navigation',
+			'label': 'World of wombats',
+			'selector': 'body > header > nav'
+		},
+		{
+			'depth': 0,
+			'role': 'main',
+			'label': 'Looking after your wombat',
+			'selector': 'body > main'
+		},
+		{
+			'depth': 1,
+			'role': 'navigation',
+			'label': 'Looking after your wombat Topics',
+			'selector': 'body > main > nav:nth-child(2)'
+		},
+		{
+			'depth': 0,
+			'role': 'contentinfo',
+			'label': null,
+			'selector': 'body > footer'
+		}
+	]
+}
+
 // Tiny helper functions
 const fixturePath = fileName => path.join(fixturesDir, fileName)
 const dataPath = fileName => path.join(dataDir, fileName)
+
+function convertCore(landmarksFormatData, testSuiteFormatData, depth) {
+	for (const landmark of testSuiteFormatData) {
+		landmarksFormatData.push({
+			'depth': depth,
+			'role': landmark.role,
+			'label': landmark.label,
+			'selector': landmark.selector
+		})
+		if (landmark.contains) {
+			convertCore(landmarksFormatData, landmark.contains, depth + 1)
+		}
+	}
+}
+
+function convertToLandmarksFormat(testSuiteFormatData) {
+	const landmarksFormatData = []
+	convertCore(landmarksFormatData, testSuiteFormatData, 0)
+	return landmarksFormatData
+}
 
 function createAllTests() {
 	const htmlFiles = fs.readdirSync(fixturesDir)
@@ -32,13 +126,20 @@ function createTest(testName, testFixture, testData) {
 		lf.find()
 		assert.deepEqual(
 			lf.allDepthsRolesLabelsSelectors(),
-			data.expected,
+			convertToLandmarksFormat(data.expected),
 			testName)
 	}
 }
 
 exports['test the damage report machine'] = function(assert) {
 	assert.ok(true, 'damage report machine intact')
+}
+
+exports['test conversion of Landmarks format results'] = function(assert) {
+	assert.deepEqual(
+		convertToLandmarksFormat(testSuiteFormatExample.expected),
+		landmarksFormatExample.expected,
+		'test data format conversion OK')
 }
 
 if (module === require.main) {
