d8e609fa41fe914e82cca6b5b6af3d35f4bdea9f
Matthew Tylee Atkinson <matatk@gmail.com>
2020-02-17 00:28:20 +0000

    New styles; Refine install/update notice for earlier versionso
    * Use the latest styles from the extension's help and options pages.
    * Make a clone of the install section rather than just repeating it.
    * Preserve and appropriately update the install/update action notices.

--- get-images-and-css-from-master.sh
+++ get-images-and-css-from-master.sh
@@ -3,3 +3,6 @@ git checkout master
 git pull
 git checkout gh-pages
 git checkout master -- 'meta/firefox*png'
+git checkout master -- 'src/static/common.css'
+mv src/static/common.css .
+rm -rfv src/static/
