matatk/landmarks

commit 20683b223cc64ebae2173ca264c79aca1534c576

test: Use the ES modules directly (#385)
This avoids the need for the generated CJS files and use of rollup in testing.

Thanks to https://remysharp.com/2019/08/16/build-free-code-and-testing-es-module-imports and https://github.com/avajs/ava/issues/2417#issuecomment-596189267 :-).

.gitignore
@@ -5,7 +5,6 @@ build
 meta/out-*.png
 landmarks-*.zip
 npm-debug.log
-generated-*.js
 trace--*.json
 times--*.json
 _site

test/rollup.config.js
@@ -1,22 +1,0 @@
-export default [{
-	input: 'src/code/contrastChecker.js',
-	output: {
-		file: 'test/generated-contrast-checker.js',
-		format: 'cjs',
-		exports: 'default'
-	}
-}, {
-	input: 'src/code/migrationManager.js',
-	output: {
-		file: 'test/generated-migration-manager.js',
-		format: 'cjs',
-		exports: 'default'
-	}
-}, {
-	input: 'src/code/landmarksFinder.js',
-	output: {
-		file: 'test/generated-landmarks-finder.js',
-		format: 'cjs',
-		exports: 'default'
-	}
-}]

test/testContrastChecker.js
@@ -1,6 +1,5 @@
-'use strict'
-const test = require('ava')
-const ContrastChecker = require('./generated-contrast-checker.js')
+import test from 'ava'
+import ContrastChecker from '../src/code/contrastChecker'
 
 const contrastChecker = new ContrastChecker()
 

test/testLandmarksFinder.js
@@ -1,8 +1,7 @@
-'use strict'
-const test = require('ava')
-const jsdom = require('jsdom')
-const pssst = require('page-structural-semantics-scanner-tests')
-const LandmarksFinder = require('./generated-landmarks-finder')
+import test from 'ava'
+import jsdom from 'jsdom'
+import pssst from 'page-structural-semantics-scanner-tests'
+import LandmarksFinder from '../src/code/landmarksFinder'
 
 const { JSDOM } = jsdom
 const checks = pssst.getFullPageTestsInline()

test/testMigrationManager.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-prototype-builtins */
-'use strict'
-const test = require('ava')
-const MigrationManager = require('./generated-migration-manager.js')
+import test from 'ava'
+import MigrationManager from '../src/code/migrationManager'
 
 // The purpose of the migration manager is to delete obsolete user settings, or
 // rename existing user settings to their current names. Landmarks ships with

commit a2d120c5f46d34c2bae02a1cd60d7a46aa1b18b4

fix(Profiling): Get the timing part working again (#406)
* Use rollup to make a version of the LandmarksFinder code that can be run via puppeteer (this had been unintentionally broken by #385).
* Rollup is only run if the source has changed since last time.
* Make the build script clearer when building a debug (as opposed to test) build.

.gitignore
@@ -11,3 +11,4 @@ _site
 .jekyll-metadata
 .nyc_output
 coverage
+scripts/wrappedLandmarksFinder.js

scripts/build.js
@@ -610,13 +610,14 @@ async function main() {
 	const action = isFullBuild ? 'Building' : 'Cleaning'
 	console.log(chalk.bold(`${action} ${extName} ${extVersion}...`))
 	const debugMode = argv.debug === true
+	const debugMsg = debugMode ? ' (debug)' : ''
 
 	testMode = argv.testRelease === true
-	const testModeMessage = testMode ? ' (test version)' : ''
+	const testMsg = testMode ? ' (test)' : ''
 
 	for (const browser of browsers) {
 		console.log()
-		logStep(chalk.bold(`${action} for ${browser}${testModeMessage}`))
+		logStep(chalk.bold(`${action} for ${browser}${testMsg}${debugMsg}`))
 		clean(browser)
 		if (isFullBuild) {
 			copyStaticFiles(browser)

scripts/profile.js
@@ -1,7 +1,9 @@
 'use strict'
 const path = require('path')
 const fs = require('fs')
+
 const puppeteer = require('puppeteer')
+const rollup = require('rollup')
 const stats = require('stats-lite')
 
 const urls = Object.freeze({
@@ -47,7 +49,7 @@ function doLandmarkInsertionRuns(sites, landmarks, runs) {
 				for (let repetition = 0; repetition < landmarks; repetition++) {
 					console.log(`Repetition ${repetition}`)
 					await insertLandmark(page, repetition)
-					await page.waitFor(delayAfterInsertingLandmark)
+					await page.waitForTimeout(delayAfterInsertingLandmark)
 				}
 
 				await page.tracing.stop()
@@ -85,8 +87,30 @@ async function insertLandmark(page, repetition) {
 // Specific landmarksFinder test
 //
 
-function timeLandmarksFinding(sites, loops) {
-	const landmarksFinderPath = path.join('scripts', 'generated-landmarks-finder.js')
+async function rollLandmarksFinder() {
+	const inputPath = path.join('src', 'code', 'landmarksFinder.js')
+	const outputPath = path.join('scripts', 'wrappedLandmarksFinder.js')
+
+	const inputModified = fs.statSync(inputPath).mtime
+	const outputModified = fs.existsSync(outputPath)
+		? fs.statSync(outputPath).mtime
+		: null
+
+	if (!fs.existsSync(outputPath) || inputModified > outputModified) {
+		console.log('Rolluping', inputPath, 'to', outputPath)
+		const bundle = await rollup.rollup({ input: inputPath })
+		await bundle.write({
+			file: outputPath,
+			format: 'cjs',
+			exports: 'default'
+		})
+	}
+
+	return outputPath
+}
+
+async function timeLandmarksFinding(sites, loops) {
+	const landmarksFinderPath = await rollLandmarksFinder()
 	const results = {}
 
 	console.log(`Runing landmarks loop test on ${sites}...`)
@@ -104,6 +128,7 @@ function timeLandmarksFinding(sites, loops) {
 			console.log(`Running landmark-finding code ${loops} times...`)
 			const durations = await page.evaluate(scanAndTallyDurations, loops)
 			results[site] = {
+				url: urls[site],
 				mean: stats.mean(durations),
 				standardDeviation: stats.stdev(durations)
 			}
@@ -141,7 +166,7 @@ function printAndSaveResults(results, loops) {
 		.replace(/:/, '')
 	const fileName = `times--${loops}--${roughlyNow}.json`
 	fs.writeFileSync(fileName, resultsJson)
-	console.log(`${fileName} written to disk.`)
+	console.log(`${fileName} written.`)
 }
 
 
@@ -161,16 +186,16 @@ async function singleRun(page, traceName, pauseBetweenClicks, postDelay) {
 		path: traceName,
 		screenshots: true
 	})
-	await page.waitFor(500)
+	await page.waitForTimeout(500)
 
 	console.log(`Clicking buttons (pause: ${pauseBetweenClicks})`)
 	for (const selector of selectors) {
 		await page.click(selector)
-		await page.waitFor(pauseBetweenClicks)
+		await page.waitForTimeout(pauseBetweenClicks)
 	}
 
 	console.log(`Waiting for ${postDelay} for page to settle`)
-	await page.waitFor(postDelay)
+	await page.waitForTimeout(postDelay)
 
 	console.log('Stopping tracing')
 	await page.tracing.stop()
@@ -201,7 +226,7 @@ function traceWithAndWithoutGuarding() {
 
 async function settle(page) {
 	console.log('Page loaded; settling...')
-	await page.waitFor(pageSettlingDelay)
+	await page.waitForTimeout(pageSettlingDelay)
 }
 
 function main() {