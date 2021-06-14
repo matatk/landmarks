7c727923f46a32b8548a9f284464236fdcdb44f5
Matthew Tylee Atkinson <matatk@gmail.com>
2021-06-14 21:54:52 +0100

    Remove top-level DevTools panel

--- src/code/_devtoolsRoot.js
+++ src/code/_devtoolsRoot.js
@@ -4,9 +4,3 @@ browser.devtools.panels.create(
 	'Landmarks',
 	'landmarks-32.png',
 	'dummyDevToolsPanel.html')
-
-browser.devtools.panels.elements.createSidebarPane(
-	'Landmarks',
-	function(sidebarPane) {
-		sidebarPane.setPage('devtoolsPanel.html')
-	})
